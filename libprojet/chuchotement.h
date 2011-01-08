a762b413b4ab564ae209572f62706e4e63d5be99
Jonathan Aceituno <join@oin.name>
2011-01-08 01:23:56 +0100

    aaaa

--- libprojet/chuchotement.h
+++ libprojet/chuchotement.h
@@ -2,11 +2,9 @@
 #define CHUCHOTEMENT_H_9BYEBXEE
 
 #include "effet.h"
-#include <son.h>
 
 struct chuchotement : public effet {
 	chuchotement(chaine_effets& s) : effet(s){}
-	size_t allongement() { return 0; }
 	virtual void operator()();
 };
 
